7e4c15d49da98e04e9eac7143ffb3d60d65c70ed
graham saltern <graham.saltern@w3partnership.com>
2013-04-24 10:57:04 +0100

    A brighter dawn

--- FirstGitJavaProject/src/com/w3/firstgitjava/HelloWorld.java
+++ FirstGitJavaProject/src/com/w3/firstgitjava/HelloWorld.java
@@ -3,5 +3,6 @@ package com.w3.firstgitjava;
 class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Brave new world!"); // Display the string.
+		System.out.println("Enter a brighter dawn"); // Display the string.
 	}
 }
